acdaaa634ce77db1a6e4e2f5f93d708237b0ccda
Artem18rus <urart88@mail.ru>
2022-09-21 09:31:44 +0400

    DZ8-1--Containers

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,24 +1,22 @@
-import { code } from 'esutils';
 import { Character, member1, member2, member3, member4, Team, eks1 } from '../task1.js';
-import { ErrorRepository, eks2 } from '../task2.js';
 
 test('TEST-OK1', () => {
   const f = () => {
     eks1.add(member1.type);
-    const obj = Object.fromEntries(eks1.members.entries())
+    const obj = Object.fromEntries(eks1.members.entries());
     return obj;
-  }
-  expect(f()).toEqual({"Zombie": "Zombie"});
+  };
+  expect(f()).toEqual({ Zombie: 'Zombie' });
 });
 
 test('TEST-OK2', () => {
   const f = () => {
     eks1.add(member3.type);
     eks1.add(member4.type);
-    const obj = Object.fromEntries(eks1.members.entries())
+    const obj = Object.fromEntries(eks1.members.entries());
     return obj;
-  }
-  expect(f()).toEqual({"Magician": "Magician", "Zombie": "Zombie", "дровосек": "дровосек"});
+  };
+  expect(f()).toEqual({ Magician: 'Magician', Zombie: 'Zombie', дровосек: 'дровосек' });
 });
 
 test('TEST-ERR1', () => {
@@ -26,30 +24,23 @@ test('TEST-ERR1', () => {
     eks1.add(member1.type);
     eks1.add(member1.type);
     return this.members;
-  }
+  };
   expect(f).toThrow('Ошибка! Такой персонаж уже в команде.');
 });
 
-// test('TEST-OK3', () => {
-//   const f = () => {
-//     const member5 = new Character();
-//     member5.type = "Men";
-//     const member6 = new Character();
-//     member6.type = "Woman";
-//     eks1.add(member5.type);
-//     eks1.add(member6.type);
-//     eks1.toArray()
-//     const obj = Object.fromEntries(eks1.members.entries())
-//     return obj;
-//   }
-//   expect(f()).toEqual({"Magician": "Magician", "Men": "Men", "Woman": "Woman", "Zombie": "Zombie", "дровосек": "дровосек"});
-// });
-
-//ЗАДАЧА 2:
-test('TEST-OK4', () => {
+test('TEST-OK3', () => {
   const f = () => {
-    eks2.translate(3);
-    return item;
+    const member5 = new Character();
+    member5.type = "Men";
+    const member6 = new Character();
+    member6.type = "Woman";
+    eks1.add(member5.type);
+    eks1.add(member6.type);
+    eks1.toArray()
+    const obj = Object.fromEntries(eks1.members.entries())
+    return obj;
   }
-  expect(f()).toBe('Ошибка #3');
+  expect(f()).toEqual({"Magician": "Magician", "Men": "Men", "Woman": "Woman", "Zombie": "Zombie", "дровосек": "дровосек"});
 });
+
+
--- src/js/task1.js
+++ src/js/task1.js
@@ -3,16 +3,12 @@ export class Character{
 }
 export const member1 = new Character();
 member1.type = "Zombie";
-
 export const member2 = new Character();
 member2.type = "Daemon";
-
 export const member3 = new Character();
 member3.type = "Magician";
-
 export const member4 = new Character();
 member4.type = "дровосек";
-
 
 export class Team {
   constructor() {
@@ -42,11 +38,11 @@ export class Team {
       return arrTotal;
     }
 }
-export const eks1 = new Team()
+export const eks1 = new Team();
 
 //eks1.add(member1.type)
 // eks1.add(member2.type)
 // eks1.add(member3.type)
 // eks1.add(member4.type)
-//eks1.addAll(member1.type, member2.type, member2.type)
+eks1.addAll(member1.type, member2.type, member2.type)
 //eks1.toArray()
